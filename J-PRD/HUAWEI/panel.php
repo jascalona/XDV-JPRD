b1b183a3cf31fbc775a106373976b7d2df8f1fc7
Jose Escalona <JEscalona@lan.xven.com.ve>
2024-04-10 08:56:48 -0400

    new

--- J-PRD/HUAWEI/panel.php
+++ J-PRD/HUAWEI/panel.php
@@ -191,7 +191,7 @@ session_start();
                                         <form action="" method="POST">
                                         <div class="container-modal-folder">
 
-                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalFolder" data-bs-whatever="@data"><a href=""></a><i class='bx bxs-edit-alt'></i></button>
+                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalFolder" data-bs-whatever="@data"><a href="./controlador/edit.php"></a><i class='bx bxs-edit-alt'></i></button>
                                             
                                             <div class="modal fade" id="modalFolder" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                             <div class="modal-dialog">
@@ -205,27 +205,27 @@ session_start();
 
                                                     <div class="mb-3">
                                                         <label for="serial" class="col-form-label">Serial</label>
-                                                        <input type="text" class="form-control" id="serial" name="serial" placeholder="Serial del equipo" required>
+                                                        <input type="text" class="form-control" id="" name="" placeholder="Serial del equipo" required>
                                                     </div>
 
                                                     <div class="mb-3">
                                                         <label for="model" class="col-form-label">Modelo</label>
-                                                        <input type="text" class="form-control" id="modelo" name="modelo" placeholder="Modelo del equipo" required>
+                                                        <input type="text" class="form-control" id="" name="" placeholder="Modelo del equipo" required>
                                                     </div>
 
                                                     <div class="mt-3">
                                                         <label for="location" class="col-form-label">Localidad</label>
-                                                        <input type="text" class="form-control" id="location" name="location" placeholder="Localidad" required>
+                                                        <input type="text" class="form-control" id="" name="" placeholder="Localidad" required>
                                                     </div>
 
                                                     <div class="mt-3">
                                                         <label for="date" class="col-form-label">Fecha</label>
-                                                        <input type="date" class="form-control" id="date" name="date" placeholder="Fecha de Instalacion" required>
+                                                        <input type="date" class="form-control" id="" name="" placeholder="Fecha de Instalacion" required>
                                                     </div>
 
                                                     <div class="mt-3">
                                                         <label for="file" class="col-form-label">File</label>
-                                                        <input type="file"class="form-control" id="file" name="file" placeholder="Examinar" required>
+                                                        <input type="file"class="form-control" id="" name="" placeholder="Examinar" required>
                                                     </div>
 
                                                         <div class="mb-3">
