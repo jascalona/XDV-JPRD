11968a2a86a539bf57de5ff7fde77c66a734cbe9
Jose Escalona <JEscalona@lan.xven.com.ve>
2024-04-11 10:47:45 -0400

    new

--- J-PRD/CUSTOMER/controlador/delete.php
+++ J-PRD/CUSTOMER/controlador/delete.php
@@ -4,7 +4,7 @@ if (!empty($_GET["id"])) {
     $id=$_GET["id"];
     $sql=$conexion->query(" delete from customer_inst where id=$id ");
     if ($sql==1) {
-        echo '<div style="position: absolute; margin-left: 550px; margin-top: 15px; " class="alert alert-success">SE HA ELIMINADO EL CAMPO</div>';
+        echo '<div style="position: absolute; margin-left: 550px; margin-top: 80px; " class="alert alert-success">SE HA ELIMINADO EL CAMPO</div>';
     } else {
         echo '<div style="position: absolute; margin-left: 550px; margin-top: 15px; class="alert alert-danger">ERROR AL ELIMINAR LOS CAMPOS</div>';
     }
